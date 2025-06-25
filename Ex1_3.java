de9c7eedca58f74676fe4b7a9dfb7eea5a011e40
MarcheurMuetV2 <remi.estaque@fms-ea.com>
2025-06-25 14:21:53 +0200

    modification de l'exo 1.2

---
+++ Ex1_3.java
@@ -0,0 +1,22 @@
+//Exo 1.3 : Multiplier chaque élément d'une liste par 2 et stocker le résultat dans une nouvelle liste
+
+public class Ex1_3 {
+    public static void main(String[] args) {
+        System.out.println();
+        // Sans stream
+        System.out.println("SANS STREAM");
+        int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int[] doubledWithoutStreamNumbers = java.util.Arrays.stream(numbers)
+                .map(n -> n * 2)
+                .toArray();
+        System.out.println("Nombres multipliés par 2 : " + java.util.Arrays.toString(doubledWithoutStreamNumbers));
+        System.out.println();
+        // Avec stream
+        System.out.println("AVEC STREAM");
+        int[] doubledStreamNumbers = new int[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            doubledStreamNumbers[i] = numbers[i] * 2;
+        }
+        System.out.println("Nombres multipliés par 2 : " + java.util.Arrays.toString(doubledStreamNumbers));
+    }
+}
---
+++ Ex1_4.java
@@ -0,0 +1,31 @@
+//Exo 1.4 : Trouver le maximum et le minimum d'une liste
+public class Ex1_4 {
+    public static void main(String[] args) {
+        int[] numbers = { 5, 12, 3, 18, 7, 50, 9, 15, 0, 11 };
+        int max = numbers[0];
+        int min = numbers[0];
+
+        System.out.println();
+        // Sans stream
+        System.out.println("AVEC STREAM");
+        int maxStream = java.util.Arrays.stream(numbers).max().orElseThrow();
+        int minStream = java.util.Arrays.stream(numbers).min().orElseThrow();
+        System.out.println("Maximum : " + maxStream);
+        System.out.println("Minimum : " + minStream);
+
+        System.out.println();
+        // Avec stream
+        System.out.println("SANS STREAM");
+
+        for (int n : numbers) {
+            if (n > max) {
+                max = n;
+            }
+            if (n < min) {
+                min = n;
+            }
+        }
+        System.out.println("Maximum : " + max);
+        System.out.println("Minimum : " + min);
+    }
+}
