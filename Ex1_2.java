3bcbb17191073d9c85afd6562d521d29b0920414
MarcheurMuetV2 <remi.estaque@fms-ea.com>
2025-06-25 14:21:24 +0200

    Exo 1 Finito

--- Ex1_2.java
+++ Ex1_2.java
@@ -5,13 +5,13 @@ import java.util.Arrays;
 
 public class Ex1_2 {
     public static void main(String[] args) {
-
         System.out.println();
         // Sans stream
         System.out.println("SANS STREAM");
         int[] numbers = { 5, 12, 3, 18, 7, 20, 9, 15, 2, 11 };
         System.out.println("Nombre d'éléments supérieurs à 10 : " +
                 Arrays.stream(numbers).filter(n -> n > 10).count());
+
         System.out.println();
         // Avec une boucle for
         System.out.println("AVEC BOUCLE FOR");
